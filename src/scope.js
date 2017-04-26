de41a787102044e9d748cfa83139459731c8da87
Nate Oliver <nate.j.oliver@gmail.com>
2017-04-26 15:17:50 -0300

    Chapter 3 Seperated Watches

--- src/scope.js
+++ src/scope.js
@@ -226,6 +226,7 @@ Scope.prototype.$new = function() {
 	var ChildScope = function() { };
 	ChildScope.prototype = this;
 	var child = new ChildScope();
+	child.$$watchers = [];
 	return child;
 };
 
